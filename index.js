1676ec49394a2dc6415f609000852393306be464
Leandro Caballero <caballero_leandro@hotmail.com>
2018-05-14 23:29:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 // write your code below!
-function happyHolidays() { return "Happy Holidays"}
-function happyHolidaysTo (name) { return `Happy holiday, $(name)!`}
-function happyHolidayTo (holiday, name) { return `Happy $(holiday), $(name)!`}
-function holidayCountdown (days, holiday) {return `It's $(days) days until $(holiday)!`}
+function happyHolidays() { return "Happy Holidays!"}
